59f95d33421fabda3acab09125c75b420878b32a
newsletter20 <hotslab20@gmail.com>
2022-01-13 11:06:49 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import spinner from './images/3d strokes 1 animated90 100px 2.35ms.webp'
 import longhighres from './images/Nov-Dec-2021/0.8/long highres.webp';
 import longlowres from './images/Nov-Dec-2021/0.8/long lowres.webp';
-import covernotitle from './images/cover background+content.png';
-import {ReactComponent as Title} from './images/cover title.svg';
+import covernotitle from './images/cover content.png';
+import { ReactComponent as Title } from './images/cover title.svg';
+import Plx from 'react-plx/lib/Plx';
+import { Controller, Scene } from "react-scrollmagic"
+import Sequence from "./ScrollSequence/Sequence";
 
 
 function App() {
@@ -11,26 +15,40 @@ function App() {
   const [isCoverLoaded, setIsCoverLoaded] = useState(false);
   const [isLongLoaded, setIsLongLoaded] = useState(true);
 
-  
+
   useEffect(() => {
     // Setting up a --vh css variable for positioning elements ignoring the address bar on mobile
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    //window.scrollTo(0,0);
   }, []);
 
   return (
     <div className="App">
-        <div className="coverContainer" style={(isCoverLoaded ? {animation: "fadein 0.5s ease-in-out both"} : {})}>
+      {/* <img className="spinner" src={spinner}/> */}
+      <Controller>
+            <Scene duration="100%" triggerHook="onLeave" pin>
+              {progress => (
+                <div style={{ height: "100vh", position: "relative" }}>
+                  <Sequence progress={progress} />
+                </div>
+              )}
+        </Scene>
+      </Controller>
+      <div className="coverContainer" style={(isCoverLoaded ? { animation: "fadein 0.5s ease-in-out both" } : {})}>
           {/* <div className="cover" id="coverlowres" onLoad={() => {setIsCoverLoaded(true)}}/> */}
-          <img className="coverN" src={covernotitle} onLoad={() => {setIsCoverLoaded(true)}}/>
-        </div>
-        <Title className="title"/>
-        <div className="longContainer" style={(isLongLoaded ? {animation: "slidein 0.5s 0.5s ease-in-out both"} : {})}>
-          <img className="long" id="longlowres" src={longlowres} onLoad={() => {setIsLongLoaded(true)}} />  
-          <img className="long" id="longhighres" src={longhighres} onLoad={() => {setIsLongLoaded(true)}} />
-        </div>
+        <img className="cover" src={covernotitle} onLoad={() => { setIsCoverLoaded(true) }} />
         
-       
+      </div>
+      <Title className="title" />
+      
+      <div className="longContainer" style={(isLongLoaded ? { height: "auto", animation: "slidein 0.5s 1s ease-in-out both" } : {})}>
+        <img className="long" id="longlowres" src={longlowres} onLoad={() => { setIsLongLoaded(true) }} />
+        <img className="long" id="longhighres" src={longhighres} onLoad={() => { setIsLongLoaded(true) }} />
+      </div>
+
+
     </div>
   );
 }
